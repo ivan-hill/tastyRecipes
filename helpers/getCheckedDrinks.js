afd057c82213b41ba586d4904671343d38ad981d
Ivan Hill <30849044+ivan-hill@users.noreply.github.com>
2022-07-07 14:27:18 +0200

    Delete helpers directory

--- helpers/getCheckedDrinks.js
+++
@@ -1,9 +0,0 @@
-const getChecked = (ingredients, id) => {
-  const inProgressKey = localStorage.getItem('inProgressRecipes');
-  if (inProgressKey && JSON.parse(inProgressKey).cocktails[id]) {
-    return JSON.parse(inProgressKey).cocktails[id].includes(ingredients);
-  }
-  return false;
-};
-
-export default getChecked;
--- helpers/getCheckedFoods.js
+++
@@ -1,9 +0,0 @@
-const getChecked = (ingredients, id) => {
-  const inProgressKey = localStorage.getItem('inProgressRecipes');
-  if (inProgressKey && JSON.parse(inProgressKey).meals[id]) {
-    return JSON.parse(inProgressKey).meals[id].some((item) => item === ingredients);
-  }
-  return false;
-};
-
-export default getChecked;
