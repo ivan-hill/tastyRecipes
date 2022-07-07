2be3e5a87b85a59a73dc5a1ca6800589de866b9d
Ivan Hill <30849044+ivan-hill@users.noreply.github.com>
2022-07-07 14:27:02 +0200

    Delete context directory

--- context/contextDrinks/contextDrinks.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const contextDrinks = createContext();
-
-export default contextDrinks;
--- context/contextFoodRecipe/contextFoodRecipe.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const contextFoodRecipe = createContext();
-
-export default contextFoodRecipe;
